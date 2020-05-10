41ef219850d3d98f09c3e2c797c05c7a6d9faeff
sanjaydub <sanjaydub@gmail.com>
2020-05-10 19:31:25 +0530

    added some more reusable steps

---
+++ vars/genAllureTestReport.groovy
@@ -0,0 +1,3 @@
+def call() {
+	allure includeProperties: false, jdk: '', properties: [[key: 'allure.issues.tracker.pattern', value: 'http://example.com/%s']], report: 'target/allure-report', results: [[path: 'target/allure-results']]
+}	
